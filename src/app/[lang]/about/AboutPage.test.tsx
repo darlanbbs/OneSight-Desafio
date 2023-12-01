d6a5477c54303e96acc511e2433a7b822d2c8842
Darlan Bomfim <darlanbs05@gmail.com>
2023-12-01 17:49:54 -0300

    :boom: Fix: Mockando a rota do useRouter do next/navigation

--- src/app/[lang]/about/AboutPage.test.tsx
+++ src/app/[lang]/about/AboutPage.test.tsx
@@ -1,3 +1,13 @@
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+    replace: jest.fn(),
+    pathname: "/",
+    query: {},
+    asPath: "/",
+  }),
+}));
+
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
@@ -5,12 +15,10 @@ import About from "./page";
 import { Locale } from "@/app/config/i18n.config";
 
 test("should render the heading", () => {
-  // Teste para lang "en-US"
   const valueEn = { lang: "en-US" as Locale };
   const { container: containerEn } = render(<About params={valueEn} />);
   expect(containerEn.innerHTML).toContain("About");
 
-  // Teste para lang "pt-BR"
   const valuePt = { lang: "pt-BR" as Locale };
   const { container: containerPt } = render(<About params={valuePt} />);
   expect(containerPt.innerHTML).toContain("Sobre");
