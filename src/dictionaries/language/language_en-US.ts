87ea4c2cae3c3ef3071d266170f4934395bed74c
Darlan Bomfim <darlanbs05@gmail.com>
2023-12-01 11:45:26 -0300

    :sparkles: Feat:Criação dos textos ficticios para testes dos idiomas

---
+++ src/dictionaries/language/language_en-US.ts
@@ -0,0 +1,7 @@
+export const enUS = {
+  site: {
+    titulo: "Welcome to My App",
+    mensagem: "This is an example message in American English.",
+    botao: "Click here",
+  },
+};
---
+++ src/dictionaries/language/language_pt-BR.ts
@@ -0,0 +1,7 @@
+export const ptBR = {
+  site: {
+    titulo: "Bem-vindo ao Meu Aplicativo",
+    mensagem: "Este é um exemplo de mensagem em português brasileiro.",
+    botao: "Clique aqui",
+  },
+};
