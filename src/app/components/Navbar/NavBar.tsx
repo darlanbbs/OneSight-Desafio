7d215abe6bb7c8a2b09ebece536209d02c7966ff
Darlan Bomfim <darlanbs05@gmail.com>
2023-12-01 17:50:40 -0300

    :sparkles: Feat: Criação da troca de idioma ao trocar de pagina

--- src/app/components/Navbar/NavBar.tsx
+++ src/app/components/Navbar/NavBar.tsx
@@ -1,9 +1,23 @@
+"use client";
 import { Locale } from "./../../config/i18n.config";
 import { getDictionarieConfigClientSide } from "../../../dictionaries/dictionariesConfigClientSide";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = ({ params }: { params: { lang: Locale } }) => {
   const dictionaries = getDictionarieConfigClientSide(params.lang);
+  const router = useRouter();
+
+  const alterarIdioma = (novoIdioma: Locale) => {
+    const urlAtual = window.location.href;
+    if (urlAtual.includes("contact" || "about")) {
+      const novaURL = urlAtual.replace(`/${params.lang}/`, `/${novoIdioma}/`);
+      router.push(novaURL);
+    }
+    const novaURL = urlAtual.replace(`/${params.lang}`, `/${novoIdioma}`);
+    router.push(novaURL);
+  };
+
   return (
     <nav className="bg-gray-800 p-4">
       <div className="flex items-center justify-between">
@@ -19,10 +33,13 @@ const Navbar = ({ params }: { params: { lang: Locale } }) => {
           </Link>
         </div>
         <div className="flex items-center">
-          <button className="text-white">
+          <button className="text-white" onClick={() => alterarIdioma("pt-BR")}>
             <img src="/icons/brazilIcon.png" alt="Brazil" className="w-6 h-6" />
           </button>
-          <button className="text-white ml-4">
+          <button
+            className="text-white ml-4"
+            onClick={() => alterarIdioma("en-US")}
+          >
             <img src="/icons/euaIcon.png" alt="EUA" className="w-6 h-6" />
           </button>
         </div>
